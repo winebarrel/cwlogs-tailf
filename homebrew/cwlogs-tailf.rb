7fb8f6ec918cf8764df63348a8336cb43547b68c
winebarrel <sgwr_dts@yahoo.co.jp>
2016-05-13 01:31:54 +0900

    Fix homebrew hash

--- homebrew/cwlogs-tailf.rb
+++ homebrew/cwlogs-tailf.rb
@@ -5,7 +5,7 @@ class CwlogsTailf < Formula
 
   homepage 'https://github.com/winebarrel/cwlogs-tailf'
   url "https://github.com/winebarrel/cwlogs-tailf/releases/download/v#{VERSION}/cwlogs-tailf-v#{VERSION}-darwin-amd64.gz"
-  sha256 'e81020b0c8059359d9b36dc9b967c41bca2f0b1a62139fe0d4979a833a2179af'
+  sha256 '00fe9a0d833005fc491e722756462d11f2b031b2936772afe06a925f3306456e'
   version VERSION
   head 'https://github.com/winebarrel/cwlogs-tailf.git', :branch => 'master'
 
