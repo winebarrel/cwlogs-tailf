73b0948917359d8df91f51009bbdb7e4465abb51
Genki Sugawara <sugawara@cookpad.com>
2016-10-11 18:26:14 +0900

    Fix version

--- homebrew/cwlogs-tailf.rb
+++ homebrew/cwlogs-tailf.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class CwlogsTailf < Formula
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 
   homepage 'https://github.com/winebarrel/cwlogs-tailf'
   url "https://github.com/winebarrel/cwlogs-tailf/releases/download/v#{VERSION}/cwlogs-tailf-v#{VERSION}-darwin-amd64.gz"
-  sha256 'b0a175ebf6ad93ba70280116249f39176a26af39a7c709b43d34cd45133c723a'
+  sha256 'e96c6109be64c64e89b3df536b1c6ec57bb4e6268941a354fed9b0ca4b38e24b'
   version VERSION
   head 'https://github.com/winebarrel/cwlogs-tailf.git', :branch => 'master'
 
