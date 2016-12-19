14b7ec128d8424bbb44496c1649d7ebbf04bf800
Genki Sugawara <sugawara@cookpad.com>
2016-12-19 14:00:20 +0900

    Fix shasum

--- homebrew/cwlogs-tailf.rb
+++ homebrew/cwlogs-tailf.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class CwlogsTailf < Formula
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 
   homepage 'https://github.com/winebarrel/cwlogs-tailf'
   url "https://github.com/winebarrel/cwlogs-tailf/releases/download/v#{VERSION}/cwlogs-tailf-v#{VERSION}-darwin-amd64.gz"
-  sha256 'e96c6109be64c64e89b3df536b1c6ec57bb4e6268941a354fed9b0ca4b38e24b'
+  sha256 '535c4e3d9accd74e5cc0994c4fd827330f42412d19cd090bd184e46d02aa8399'
   version VERSION
   head 'https://github.com/winebarrel/cwlogs-tailf.git', :branch => 'master'
 
